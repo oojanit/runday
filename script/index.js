096589669bd61e004f57c06f18f4aec522f50e2b
Bon8 <oojanit@naver.com>
2025-06-16 11:47:10 +0900

    .

--- script/index.js
+++ script/index.js
@@ -1,117 +1,125 @@
 // index.js
 
-$(function(){
+$(function () {
 
-// gnb submenu dropdown
-    $('.submenu').on('mouseenter',function(){
+    // gnb submenu dropdown
+    $('.submenu').on('mouseenter', function () {
         $('.gnb .submenu ul').stop().slideDown(200);
     });
-    $('.submenu').on('mouseleave',function(){
+    $('.submenu').on('mouseleave', function () {
         $('.gnb .submenu ul').stop().slideUp(200);
     });
 
-// main img slide + catch text change
+    // main img slide + catch text change
     var catch1 = $('.catch').html();
     var catch2 = '<h2>오직 나만을 위한 <br>달리기 전문 트레이너!</h2><p>달리기는 외로운 운동이다?<br>여러분은 혼자가 아닙니다!<br>나만의 100% 풀 보이스 트레이너와 함께 달려보는 건 어떨까요?</p>';
     var page = 0;
-    function mainSlide (){
+    function mainSlide() {
         page++;
-        $('.catch').css('display','none');
+        $('.catch').css('display', 'none');
         if (page === 2) {
             $('.catch').html(catch1).fadeIn(700);
         } else if (page === 3) {
             page = 1;
-            $('.main_box').css('margin-left','0');
+            $('.main_box').css('margin-left', '0');
             $('.catch').html(catch2).fadeIn(700);
         } else if (page === 1) {
             $('.catch').html(catch2).fadeIn(700);
         }
         $('.main_box').animate({
             marginLeft: -page * ($('.main_img').width())
-        },700);
+        }, 700);
     }
-    setInterval(mainSlide,3000);    
+    setInterval(mainSlide, 3000);
 
-// promo slide in 900, 1400
+    // promo slide in 900, 1400
     var scTop = 0;
-    $(window).on('scroll',function(){
+    $(window).on('scroll', function () {
         scTop = $(this).scrollTop();
         /* console.log(scTop); */
         if (scTop >= 400) {
             $('.promo h3').animate({
                 opacity: 1
-            },600);
+            }, 600);
         };
         if (scTop >= 900) {
             $('.left .cardimg').animate({
                 opacity: 1
-            },600)
+            }, 600)
             $('.left .desc').animate({
                 marginTop: '10%',
                 opacity: 1
-            },300);
+            }, 300);
         }
         if (scTop >= 1400) {
             $('.right .cardimg').animate({
                 opacity: 1
-            },600)
+            }, 600)
             $('.right .desc').animate({
                 marginTop: '10%',
                 opacity: 1
-            },300);
+            }, 300);
         }
 
-// score pop up 2100~
+        // score pop up 2100~
         if (scTop >= 2100) {
             $('.pop').eq(0).animate({
                 opacity: 1,
                 backgroundPositionY: '200px'
-            },500);
+            }, 500);
         }
         if (scTop >= 2300) {
             $('.pop').eq(1).animate({
                 opacity: 1,
                 backgroundPositionY: '200px'
-            },500);
+            }, 500);
         }
         if (scTop >= 2400) {
             $('.pop').eq(2).animate({
                 opacity: 1,
                 backgroundPositionY: '200px'
-            },500);
+            }, 500);
         }
         if (scTop >= 2600) {
             $('.pop').eq(3).animate({
                 opacity: 1,
                 backgroundPositionY: '200px'
-            },500);
+            }, 500);
         }
 
-    });    
-
-// marquee
-    /* $('.mrq1').marquee({
-        speed: 100,
-	    gap: 20, //gap in pixels between the tickers
-	    delayBeforeStart: 0, //time in milliseconds before the marquee will start animating
-	    direction: 'right', //'left' or 'right'
-	    duplicated: true, //true or false - should the marquee be duplicated to show an effect of continues flow
-        startVisible: true
     });
-    $('.mrq2').marquee({
-        speed: 100,
-	    gap: 20,
-	    delayBeforeStart: 0,
-	    direction: 'left',
-	    duplicated: true,
-        startVisible: true
-    }); */
-    // 스크롤 내렸을 때 새로고침 되듯이 위로 스크롤 돌아가는 현상 수정하기
 
-// media image slide
-    $('.slidebtn .btn_prev').click(function(e){
+    // marquee
+    setInterval(flow,10);
+    var moveNum = 0; // 이동하는 left값을 담을 변수!
+
+    // 함수 flow() 기능: 컨텐츠를 왼쪽으로 흐르게 함
+    function flow() {
+        moveNum++; // left 이동값을 1씩 증가
+
+        // (무한반복을 위해) 다음을 위한 준비!
+        // li 하나의 너비를 구함: 그 너비보다 이동한 left값(moveNum)이 커졌을 때 
+        var boxWidth = $('.mrq1 .txt').first().outerWidth();
+        if (moveNum > boxWidth) {
+            // 이동한 픽셀수가 li 하나의 너비보다 커졌을 때! >> 다음을 위한 준비!
+            // (왼쪽으로 흘러가서) 사라진 첫번째 li를 .flow의 맨 뒤로 이동 >> append
+            // .flow의 li 순서가 변경되었으므로 left값 초기화!
+            // 동시에 이동값(moveNum) 초기화        
+            $('.mrq1').append($('.mrq1 .txt').first()).css({
+                marginLeft: 0
+            });
+            moveNum = 0;
+        } else { // 실제로 움직이기
+            $('.mrq1').css({
+                marginLeft: -moveNum
+            });
+        }
+    }
+
+    // media image slide
+    $('.slidebtn .btn_prev').click(function (e) {
         e.preventDefault();
-        
+
         /* $('.slidebox .desc').slideUp(300);
         $('.slidebox .desc').slideDown(300); */
 
@@ -120,7 +128,7 @@ $(function(){
         $('.slider.prev').children('.slideimg').last().prependTo($('.slider.next'));
     });
 
-    $('.slidebtn .btn_next').click(function(e){
+    $('.slidebtn .btn_next').click(function (e) {
         e.preventDefault();
 
         /* $('.slidebox .desc').slideUp(300);
