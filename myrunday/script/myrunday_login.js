4800602020ef709b5413d671aecef3e775d4fdf7
Bon8 <oojanit@naver.com>
2025-06-24 22:07:23 +0900

    .

--- myrunday/script/myrunday_login.js
+++ myrunday/script/myrunday_login.js
@@ -1,32 +1,42 @@
 // myrunday_login.js
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+
     // submenu slidedown
-    $('.gnb li:last-child').on('mouseenter',function(){
+    $('.gnb').on('mouseenter', function () {
         $('.sub').stop().slideDown(300);
     });
-    $('.gnb').on('mouseleave',function(){
-        if($('.gnb li:last-child').hasClass('on')){
+    $('.gnb').on('mouseleave', function () {
+        if ($('.gnb li:last-child').hasClass('on')) {
 
         } else {
             $('.sub').stop().slideUp(300);
-        }        
+        }
     });
 
     // selected menu li.on design
-    $('.gnb > li > a').on('click',function(){
+    $('.gnb > li > a').on('click', function () {
         $(this).parent('li').addClass('on').siblings().removeClass('on');
     });
-    
+
     // #loginform / #joinform toggle
     $('#joinform').hide();
-    $('form > p > a').on('click',function(e){
+    $('form > p > a').on('click', function (e) {
         e.preventDefault();
         $('#loginform').slideToggle(400);
         $('#joinform').slideToggle(400);
     });
 
-    // button hover effect
+    // button hover effect - script or css?
+    $('form input[type="submit"]').on('mouseenter',function(){
+        $(this).stop().css({
+            backgroundColor: 'rgba(90, 74, 248, 0.4)'
+        });
+    });
+    $('form input[type="submit"]').on('mouseleave',function(){
+        $(this).stop().css({
+            backgroundColor: 'rgb(90,74,248)'
+        },300);
+    });
 
 });
